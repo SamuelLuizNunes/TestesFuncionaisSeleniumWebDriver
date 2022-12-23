8812ac9637747836b1ad7d33c87768c8751f485b
Samuel Nunes <samuelluiznunes10@gmail.com>
2022-12-23 19:07:30 -0300

    textFild

--- src/main/java/br/com/automatizando/TesteCampoDeTreinamento.java
+++ src/main/java/br/com/automatizando/TesteCampoDeTreinamento.java
@@ -3,6 +3,7 @@ package br.com.automatizando;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoDeTreinamento {
@@ -13,6 +14,8 @@ public class TesteCampoDeTreinamento {
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de escrita");
+        Assert.assertEquals("Teste de escrita", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 
         driver.quit();
     }
